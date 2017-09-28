656a0108e5a6273c37b04e2b2e3ed6add3253cc4
Maciej Nowicki <maciej@nowicki.com>
2017-09-28 20:03:19 +0200

    Better logs

--- src/main/scala/onvifsnapshottaker/photo/FfmpegPhotoMaker.scala
+++ src/main/scala/onvifsnapshottaker/photo/FfmpegPhotoMaker.scala
@@ -11,12 +11,14 @@ object FfmpegPhotoMaker extends PhotoMaker {
   private val config = Config().getConfig("ffmpeg-photo-maker")
   private val script = Paths.get(config.getString("script"))
   private val image = Paths.get(config.getString("image"))
+  private val log = Paths.get(config.getString("log"))
 
   override def shot(): Try[Array[Byte]] = {
     Try {
-      val process = Runtime.getRuntime.exec(s"${script.toString} | tee ffmpeg.log")
+      val process = Runtime.getRuntime.exec(s"${script.toString} | tee ${log.toAbsolutePath.toString}")
       process.waitFor()
       val bytes = Files.readAllBytes(image)
+      Files.delete(image)
       process.destroy()
       bytes
     }
